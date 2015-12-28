662aa9f569a04e28163af16bed9ddb28dadb728d
Christoffer Henriksson <tottech@gmail.com>
2015-12-28 11:46:50 +0100

    Added indication when TouchID ready

--- Tweak.m
+++ Tweak.m
@@ -199,6 +199,11 @@ void touchIDFail(CFNotificationCenterRef center,
     CFNotificationCenterAddObserver(CFNotificationCenterGetDarwinNotifyCenter(), (void*)observer, &touchIDSuccess, CFSTR("net.tottech.banktouch/success"), NULL, 0);
     CFNotificationCenterAddObserver(CFNotificationCenterGetDarwinNotifyCenter(), (void*)observer, &touchIDFail, CFSTR("net.tottech.banktouch/failure"), NULL, 0);
     CFNotificationCenterPostNotification(CFNotificationCenterGetDarwinNotifyCenter(), CFSTR("net.tottech.banktouch/startMonitoring"), nil, nil, YES);
+    
+    codeTextField.placeholder = @"Security Code or TouchID";
+    codeTextField.layer.borderColor = [UIColor greenColor].CGColor;
+    codeTextField.layer.borderWidth = 1;
+    codeTextField.layer.cornerRadius = 5;
 }
 
 %end
